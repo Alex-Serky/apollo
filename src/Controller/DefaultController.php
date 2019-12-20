b7e8b1af14856123da1f4f5bbccf51342cf6cc29
Alex Serky <aserky@gmail.com>
2019-12-21 00:25:16 +0100

    Events & Listeners base concepts & Create listener

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -26,66 +26,7 @@ class DefaultController extends AbstractController
     {
         // $entityManager = $this->getDoctrine()->getManager();
 
-        $cache = new TagAwareAdapter(
-            new FilesystemAdapter()
-        );
-
-        $acer = $cache->getItem('acer');
-        $asus = $cache->getItem('asus');
-        $dell = $cache->getItem('dell');
-        $ibm = $cache->getItem('ibm');
-        $apple = $cache->getItem('apple');
-
-        if (!$asus->isHit()) {
-            $asus_from_db = 'asus laptop';
-            $asus->set($asus_from_db);
-            $asus->tag(['computers', 'laptops', 'asus']);
-            $cache->save($asus);
-            dump('asus laptop from database ...');
-        }
-
-        if (!$ibm->isHit()) {
-            $ibm_from_db = 'ibm desktop';
-            $ibm->set($ibm_from_db);
-            $ibm->tag(['computers', 'desktops', 'ibm']);
-            $cache->save($ibm);
-            dump('ibm desktop from database ...');
-        }
-
-        if (!$dell->isHit()) {
-            $dell_from_db = 'dell laptop';
-            $dell->set($dell_from_db);
-            $dell->tag(['computers', 'laptops', 'dell']);
-            $cache->save($dell);
-            dump('dell laptop from database ...');
-        }
-
-        if (!$acer->isHit()) {
-            $acer_from_db = 'acer desktop';
-            $acer->set($acer_from_db);
-            $acer->tag(['computers', 'desktops', 'acer']);
-            $cache->save($acer);
-            dump('acer desktop from database ...');
-        }
-
-        if (!$apple->isHit()) {
-            $apple_from_db = 'apple desktop';
-            $apple->set($apple_from_db);
-            $apple->tag(['computers', 'desktops', 'apple']);
-            $cache->save($apple);
-            dump('apple desktop from database ...');
-        }
-
-        $cache->invalidateTags(['ibm']);
-        $cache->invalidateTags(['desktops']);
-        $cache->invalidateTags(['computers']);
-        $cache->invalidateTags(['laptops']);
-
-        dump($acer->get());
-        dump($dell->get());
-        dump($ibm->get());
-        dump($apple->get());
-
+        dump($request, $this);
 
         return $this->render('default/index.html.twig', [
             'controller_name' => 'Le Contrôleur'
---
+++ src/Services/KernelResponseListener.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Services;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+
+class KernelResponseListener
+{
+
+    public function onKernelResponse(FilterResponseEvent $event)
+    {
+        $response = new Response('dupa');
+        $event->setResponse($response);
+    }
+}
